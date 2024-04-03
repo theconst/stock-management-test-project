d76e53cbe874868d17c0813e0f153d500b8bec06
kko <k.kovalchuk2017@gmail.com>
2024-04-03 10:08:45 -0400

    [TEST][SCAFFOLD] Add stubs for purchases and sales

--- sales/src/main/java/com/eclub/Main.java
+++ sales/src/main/java/com/eclub/Main.java
@@ -2,6 +2,6 @@ package com.eclub;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola eclub!");
+        System.out.println("Hello sales!");
     }
 }
