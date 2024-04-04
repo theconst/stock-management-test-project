6c91d1ff7d49cce7af17c1ede90fdc9d770a8aa6
kko <k.kovalchuk2017@gmail.com>
2024-04-03 23:36:30 -0400

    [TEST][DOC] Move script doc to README.MD

--- scripts/put_products.sh
+++ scripts/put_products.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-#
-# Script for add hoc pushing of data to the API
-#
-# Integration tests already cover the functionality.
-#
-# The script may be used if manual interaction in local is needed
 
 PRODUCTS_ENDPOINT_URL="${PRODUCTS_ENDPOINT_URL:-localhost:8080/products}"
 
