17e136446cfe3bc68a1d6b2ba0f02b6d8fea7c02
kko <k.kovalchuk2017@gmail.com>
2024-04-08 10:29:06 -0400

    [TEST][TEST] Fix files

--- purchases/src/test/resources/schema.sql
+++ purchases/src/test/resources/schema.sql
@@ -1,3 +1,2 @@
 RUNSCRIPT FROM 'classpath:db/migration/V1_0_0__initial_schema_purchases.sql'
-RUNSCRIPT FROM 'classpath:db/migration/V1_0_0__initial_schema_sales.sql'
-RUNSCRIPT FROM 'classpaht:db/migration/V1_0_3__remove_from_stock_outbox.sql'
+RUNSCRIPT FROM 'classpath:db/migration/V1_0_2__stock_operations.sql'
