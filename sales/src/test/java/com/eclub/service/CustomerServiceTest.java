249283fc1d21e5316574caa94685c9e8d59763fb
kko <k.kovalchuk2017@gmail.com>
2024-04-07 22:28:21 -0400

    [TEST][TEST] Add customer service test

---
+++ sales/src/test/java/com/eclub/service/CustomerServiceTest.java
@@ -0,0 +1,30 @@
+package com.eclub.service;
+
+import com.eclub.ServiceTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ServiceTest
+class CustomerServiceTest {
+
+    @Test
+    void shouldCreateCustomer() {
+        fail("Not implemented");
+    }
+
+    @Test
+    void shouldUpdateCustomer() {
+        fail("Not implemented");
+    }
+
+    @Test
+    void shouldGetCustomerById() {
+        fail("Not implemented");
+    }
+
+    @Test
+    void shouldListCustomers() {
+        fail("Not implemented");
+    }
+}
