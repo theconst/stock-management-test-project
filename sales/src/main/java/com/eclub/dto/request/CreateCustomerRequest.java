2d9fb2c34f2c38bfa463d4bacab2d4fb3d27e228
kko <k.kovalchuk2017@gmail.com>
2024-04-08 10:21:17 -0400

    [TEST][IMPL] Not blank for customer names

--- sales/src/main/java/com/eclub/dto/request/CreateCustomerRequest.java
+++ sales/src/main/java/com/eclub/dto/request/CreateCustomerRequest.java
@@ -1,16 +1,16 @@
 package com.eclub.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.extern.jackson.Jacksonized;
 
 @Builder
 @Jacksonized
 public record CreateCustomerRequest(
-        @NotNull
+        @NotBlank
         @Schema(description = "First name", example = "John") String firstName,
-        @NotNull
+        @NotBlank
         @Schema(description = "Last name", example = "Smith") String lastName,
         @Schema(description = "Phone number", example = "+123456789") String phoneNumber,
         @Schema(description = "Customer address", example = "Green Str.") String address) {
