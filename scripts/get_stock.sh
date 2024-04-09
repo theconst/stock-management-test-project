74e3fc54cafe10b4448b61aa27efab6ce02bba4f
kko <k.kovalchuk2017@gmail.com>
2024-04-09 13:21:33 -0400

    [TEST][TEST] Fix scripts

--- scripts/get_stock.sh
+++ scripts/get_stock.sh
@@ -1,3 +1,3 @@
-STOCK_ITEMS_ENDPOINT="${STOCK_ITEMS_ENDPOINT:-localhost:8080/stock-items/?page=0&size=1}"
+STOCK_ITEMS_ENDPOINT="${STOCK_ITEMS_ENDPOINT:-localhost:8080/stock-items/?pageNumber=0&pageSize=1}"
 
 curl -XGET "$STOCK_ITEMS_ENDPOINT" | jq .
